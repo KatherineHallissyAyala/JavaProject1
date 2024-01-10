43e2fed5116635ff5b872255dbf0ac98617bd350
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2024-01-10 14:01:37 -0500

    First Commit

---
+++ QuickStart.java
@@ -0,0 +1,15 @@
+class QuickStart {
+    public static void main (String[] args) {
+        int quantity = 5;
+        double rate = 3.4;
+        boolean veracity = false;
+        char initial = 'j';
+        String greeting = "Happy New Year";
+        System.out.println("Hello, World.  Java is here.");
+        System.out.println(quantity);
+        System.out.println(rate);
+        System.out.println(veracity);
+        System.out.println(initial);
+        System.out.println(greeting);
+    }
+}
