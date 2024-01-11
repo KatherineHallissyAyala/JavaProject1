f6e9564faec5a3bf89ae49233a349e17335abeb6
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2024-01-11 11:29:59 -0500

    playing with user input

--- QuickStart.java
+++ QuickStart.java
@@ -1,15 +1,9 @@
+import java.util.Scanner;
+
 class QuickStart {
     public static void main (String[] args) {
-        int quantity = 5;
-        double rate = 3.4;
-        boolean veracity = false;
-        char initial = 'j';
-        String greeting = "Happy New Year";
-        System.out.println("Hello, World.  Java is here.");
-        System.out.println(quantity);
-        System.out.println(rate);
-        System.out.println(veracity);
-        System.out.println(initial);
-        System.out.println(greeting);
+        Scanner userResponse = new Scanner(System.in);
+        String scanned = userResponse.next();
+        System.out.println(scanned);
     }
 }
