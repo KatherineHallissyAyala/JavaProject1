a34efc056057a1def666c10b1307357e14327c9f
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2024-01-11 11:51:27 -0500

    playing with logic

--- QuickStart.java
+++ QuickStart.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 class QuickStart {
     public static void main (String[] args) {
-        Scanner userResponse = new Scanner(System.in);
-        String scanned = userResponse.next();
-        System.out.println(scanned);
+        int x = 7;
+        int y = 20;
+        int z = 7;
+
+        boolean compare = x > y;
+
+        System.out.println(compare);
+        //Scanner userResponse = new Scanner(System.in);
+        // String scanned = userResponse.next();
+        // System.out.println(scanned);
     }
 }
